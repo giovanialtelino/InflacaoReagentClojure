3cbbaed9a7324bb1e8fe71a1a3a48a021e7a78ec
giovanialtelino <giovanialtelino@hotmail.com>
2020-03-09 10:52:11 -0300

    component ready to deploy

--- inflacao-pedestal-service/src/inflacao_pedestal_service/components/database.clj
+++ inflacao-pedestal-service/src/inflacao_pedestal_service/components/database.clj
@@ -5,7 +5,7 @@
 
 ;;Of course you should never use your password like this,
 ;a decent idea would have those normal files here and them use environ and System to assoc the password value to this map
-(def datatasource-config {:auto-commit        true
+(def datatasource-dev {:auto-commit        true
                           :read-only          false
                           :connection-timeout 30000
                           :validation-timeout 5000
@@ -18,6 +18,23 @@
                           :username           "docker"
                           :password           "docker"
                           :database-name      "postgres"
+                          :server-name        "localhost"
+                          :port-number        5432
+                          :register-mbeans    false})
+
+(def datatasource-config {:auto-commit        true
+                          :read-only          false
+                          :connection-timeout 30000
+                          :validation-timeout 5000
+                          :idle-timeout       600000
+                          :max-lifetime       1800000
+                          :minimum-idle       10
+                          :maximum-pool-size  10
+                          :pool-name          "db-pool"
+                          :adapter            "postgresql"
+                          :username           "postgres"
+                          :password           "pedestalTHISisVERT"
+                          :database-name      "inflacao"
                           :server-name        "localhost"
                           :port-number        5432
                           :register-mbeans    false})
